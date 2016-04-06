b08f33e33b8c958a33f74105ba9b948d2660a7c4
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-04-06 10:13:40 +0200

    fixed: use shallow clones on jenkins

--- jenkins/build-opm-output.sh
+++ jenkins/build-opm-output.sh
@@ -7,7 +7,7 @@ function build_opm_output {
   cd $WORKSPACE/deps/ert
   git init .
   git remote add origin https://github.com/Ensembles/ert
-  git fetch origin $ERT_REVISION:branch_to_build
+  git fetch --depth 1 origin $ERT_REVISION:branch_to_build
   test $? -eq 0 || exit 1
   git checkout branch_to_build
   popd
@@ -16,7 +16,9 @@ function build_opm_output {
   mkdir -p serial/build-ert
   cd serial/build-ert
   cmake $WORKSPACE/deps/ert/devel -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install
+  test $? -eq 0 || exit 1
   cmake --build . --target install
+  test $? -eq 0 || exit 1
   popd
 
   # Build opm-common
@@ -25,7 +27,7 @@ function build_opm_output {
   cd $WORKSPACE/deps/opm-common
   git init .
   git remote add origin https://github.com/OPM/opm-common
-  git fetch origin $OPM_COMMON_REVISION:branch_to_build
+  git fetch --depth 1 origin $OPM_COMMON_REVISION:branch_to_build
   test $? -eq 0 || exit 1
   git checkout branch_to_build
   popd
@@ -35,21 +37,26 @@ function build_opm_output {
   mkdir serial/build-opm-common
   cd serial/build-opm-common
   build_module "-DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" 0 $WORKSPACE/deps/opm-common
+  test $? -eq 0 || exit 1
   popd
 
   # Build opm-parser
   clone_and_build_module opm-parser "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $OPM_PARSER_REVISION $WORKSPACE/serial
+  test $? -eq 0 || exit 1
 
   # Build opm-material
   clone_and_build_module opm-material "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $OPM_MATERIAL_REVISION $WORKSPACE/serial
+  test $? -eq 0 || exit 1
 
   # Build opm-core
   clone_and_build_module opm-core "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $OPM_CORE_REVISION $WORKSPACE/serial
+  test $? -eq 0 || exit 1
 
   # Build opm-output
   pushd .
   mkdir serial/build-opm-output
   cd serial/build-opm-output
   build_module "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install" 1 $WORKSPACE
+  test $? -eq 0 || exit 1
   popd
 }
