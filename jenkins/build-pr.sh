920762e7fc66b218d001316a026ad2a5368dd74e
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-08-22 13:42:24 +0200

    simplify jenkins build scripts after ert reorganization

--- jenkins/build-pr.sh
+++ jenkins/build-pr.sh
@@ -3,18 +3,14 @@
 source `dirname $0`/build-opm-output.sh
 
 declare -a upstreams
-upstreams=(opm-parser)
+upstreams=(ert
+           opm-parser)
 
 declare -A upstreamRev
+upstreamRev[ert]=master
 upstreamRev[opm-parser]=master
 
-ERT_REVISION=master
 OPM_COMMON_REVISION=master
-
-if grep -q "ert=" <<< $ghprbCommentBody
-then
-  ERT_REVISION=pull/`echo $ghprbCommentBody | sed -r 's/.*ert=([0-9]+).*/\1/g'`/merge
-fi
 
 if grep -q "opm-common=" <<< $ghprbCommentBody
 then
@@ -29,7 +25,7 @@ do
   fi
 done
 
-echo "Building with ert=$ERT_REVISION opm-common=$OPM_COMMON_REVISION opm-parser=${upstreamRev[opm-parser]} opm-output=$sha1"
+echo "Building with opm-common=$OPM_COMMON_REVISION ert=${upstreamRev[ert]} opm-parser=${upstreamRev[opm-parser]} opm-output=$sha1"
 
 build_opm_output
 test $? -eq 0 || exit 1
