ca3d9e2fc43f043ea2e03545c5a069e77b7bad12
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-11-29 21:51:45 +0100

    update jenkins downstream list
    opm-grid moved in the dependency graph

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -18,8 +18,8 @@ fi
 # Downstream revisions
 declare -a downstreams
 downstreams=(opm-material
-             opm-core
              opm-grid
+             opm-core
              ewoms
              opm-simulators
              opm-upscaling)
